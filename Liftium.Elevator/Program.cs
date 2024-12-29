112fce378b49d4f1fa7c71360c987a0486a336cc
Telepov Denis <telepov.d@promail366.com>
2024-12-29 12:43:47 +0700

    init web api

---
+++ Liftium.Elevator/Program.cs
@@ -0,0 +1,22 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddSwaggerGen();
+}
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
